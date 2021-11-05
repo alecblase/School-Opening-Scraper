cee92b52defabed07f2391fcc1b051f88a28d55f
Saba Ali <34684242+SabaAli@users.noreply.github.com>
2021-11-04 23:39:05 -0400

    Update main.py
    Remove states that are no longer updating / active from currentStates list

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ if __name__ == '__main__':
     # disabledStates = ["arizona", "virginia"]
     #logging.basicConfig(filename='app.log', filemode='a', format='%(asctime)s - %(message)s', level=logging.INFO)
     # Currently working states <-- ADD STATES BELOW
-    currentStates = ["alabama", "colorado", "illinois", "new_mexico", "maryland", "ohio", "oregon", "south_carolina",
-                     "tennessee", "washington", "north_carolina", "connecticut", "rhode_island", "virginia", "hawaii", 
-                     "missouri", "idaho", "louisiana", "new_york"]
+    currentStates = ["alabama", "illinois", "south_carolina",
+                     "tennessee", "north_carolina", "rhode_island",
+                     "louisiana"]
 
     # Run with Box integration flag (defaults to True)
     useBox = True
